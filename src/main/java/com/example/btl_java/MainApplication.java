77f7f05fa027f2ae742009214d088e30a8c4f826
Minh Quân <anhquankk51@gmail.com>
2022-09-19 13:03:04 +0700

    update

--- src/main/java/com/example/btl_java/MainApplication.java
+++ src/main/java/com/example/btl_java/MainApplication.java
@@ -3,21 +3,22 @@ package com.example.btl_java;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("fxml/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Hello!");
+        stage.getIcons().add(new Image("images/icon.png"));
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.example.btl_java {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
-
-    opens com.example.btl_java to javafx.fxml;
+    opens images;
+    opens com.example.btl_java.controllers to javafx.fxml;
     exports com.example.btl_java;
     exports com.example.btl_java.controllers;
-    opens com.example.btl_java.controllers to javafx.fxml;
 }
