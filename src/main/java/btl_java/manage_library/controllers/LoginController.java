bb8d31fc4ba4c5469e6a9ad5c8c2dfa7a55cb5f4
hquang782 <haquang782002@gmail.com>
2022-09-22 22:37:37 +0700

    22/09

--- src/main/java/btl_java/manage_library/controllers/LoginController.java
+++ src/main/java/btl_java/manage_library/controllers/LoginController.java
@@ -2,25 +2,24 @@ package btl_java.manage_library.controllers;
 
 
 import btl_java.manage_library.MainApplication;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.awt.*;
 import java.io.IOException;
 
 public class LoginController {
     public Stage stage;
     @FXML
     TextField PassWord;
-    public void launchLogin(Stage primaryStage) throws IOException {
-        System.out.println(primaryStage);
-        this.stage = primaryStage;
+    @FXML
+    Label lblResult;
+    public void launchLogin(Stage stage) throws IOException {
+        this.stage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/fxml/login-app.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         this.stage.setTitle("Login");
@@ -32,6 +31,10 @@ public class LoginController {
     public void btn_submit() throws IOException {
         if(PassWord.getText().equals("1234")){
             new HomeController().displayHome(MainApplication.primaryStage);
+        }
+        else{
+            lblResult.setTextFill(Color.RED);
+            lblResult.setText("mật khẩu sai vui lòng nhập lại");
         }
     }
 }
