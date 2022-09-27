6cc09912c52bca6767ee3f83676faf977dc93e59
Minh Quân <anhquankk51@gmail.com>
2022-09-27 18:39:25 +0700

    Quân

--- src/main/java/btl_java/manage_library/MainApplication.java
+++ src/main/java/btl_java/manage_library/MainApplication.java
@@ -4,13 +4,16 @@ import btl_java.manage_library.controllers.LoginController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import javax.swing.table.TableRowSorter;
+import java.io.IOException;
 
 
 public class MainApplication extends Application {
     public static Stage primaryStage;
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage) throws IOException {
         primaryStage = stage;
+        stage.setMaximized(true);
         new LoginController().launchLogin(stage);
     }
     public static void main(String[] args) {
--- src/main/java/btl_java/manage_library/controllers/HomeController.java
+++ src/main/java/btl_java/manage_library/controllers/HomeController.java
@@ -11,8 +11,7 @@ public class HomeController {
 
     public void displayHome(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/fxml/home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
-        stage.setTitle("Quản lý sinh viên ");
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
