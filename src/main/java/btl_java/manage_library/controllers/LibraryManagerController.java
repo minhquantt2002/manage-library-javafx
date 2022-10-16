72cc7c93b1541d8f0f42c92794f8d9f33ed1d11a
Minh Quân <anhquankk51@gmail.com>
2022-10-16 10:36:32 +0700

    Quân-Ae pull code về nhớ

--- src/main/java/btl_java/manage_library/controllers/LibraryManagerController.java
+++ src/main/java/btl_java/manage_library/controllers/LibraryManagerController.java
@@ -8,6 +8,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
 import java.net.URL;
+import java.time.temporal.ValueRange;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class LibraryManagerController implements Initializable {
@@ -18,41 +20,50 @@ public class LibraryManagerController implements Initializable {
     public TextField sdtStfield;
     @FXML
     private TableView<LibraryModel> tableView;
-    //    @FXML
-//    private TableColumn<LibraryModel, Date> timeIn;
-//    @FXML
-//    private TableColumn<LibraryModel, Date> timeOut;
     @FXML
-    private TableColumn<LibraryModel, String> nameStudent;
+    private TableColumn<LibraryModel, String> timeIn;
+    @FXML
+    private TableColumn<LibraryModel, String> timeOut;
     @FXML
     private TableColumn<LibraryModel, String> codeStudent;
     @FXML
-    private TableColumn<LibraryModel, String> bookTook;
-    public Button btnChangePassword;
+    private TableColumn<LibraryModel, String> nameStudent;
+    @FXML
+    private TableColumn<LibraryModel, String> classStudent;
+    @FXML
+    private TableColumn<LibraryModel, String> phoneNumber;
+    @FXML
+    private TableColumn<LibraryModel, String> stt;
 
     @FXML
     // TODO: Quân
     public void initialize(URL url, ResourceBundle rb) {
-        // set theo getter ấy
+        stt.setCellValueFactory(cellData -> cellData.getValue().getStt());
         codeStudent.setCellValueFactory(cellData -> cellData.getValue().getCodeStudent());
         nameStudent.setCellValueFactory(cellData -> cellData.getValue().getNameStudent());
-        bookTook.setCellValueFactory(cellData -> cellData.getValue().getBookTook());
+        classStudent.setCellValueFactory(cellData -> cellData.getValue().getClassStudent());
+        phoneNumber.setCellValueFactory(cellData -> cellData.getValue().getPhoneNumber());
+        timeIn.setCellValueFactory(cellData -> cellData.getValue().getTimeIn());
+        timeOut.setCellValueFactory(cellData -> cellData.getValue().getTimeOut());
 
         ObservableList<LibraryModel> list = getDataList();
         tableView.setItems(list);
-
-        for (LibraryModel a : list) {
-            System.out.println(a);
-        }
     }
+
     private ObservableList<LibraryModel> getDataList() {
-//        LibraryModel row1 = new LibraryModel(new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()), "B20dccn553", "Quan", "Nhat ki trong tu");
-//        LibraryModel row2 = new LibraryModel(new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()), "B20dccn553", "Quan", "Nhat ki trong tu");
-//        LibraryModel row3 = new LibraryModel(new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()), "B20dccn553", "Quan", "Nhat ki trong tu");
-        LibraryModel test = new LibraryModel("B20dccn553", "Quan1", "Nhat ki trong tu1");
-        LibraryModel test1 = new LibraryModel("B20dccn553", "Quan2", "Nhat ki trong tu2");
-        LibraryModel test2 = new LibraryModel("B20dccn553", "Quan3", "Nhat ki trong tu3");
-        return FXCollections.observableArrayList(test, test1, test2);
+        LibraryModel test = new LibraryModel(1, "B20dccn553", "Quan1", "0333935933", "D20DCQCN01-B");
+        LibraryModel test1 = new LibraryModel(2, "B20dccn553", "Quan2", "0333935933", "D20DCQCN01-B");
+        LibraryModel test2 = new LibraryModel(3, "B20dccn553", "Quan3", "0333935933", "D20DCQCN01-B");
+        LibraryModel test3 = new LibraryModel(4, "B20dccn553", "Quan4", "0333935933", "D20DCQCN01-B");
+        LibraryModel test4 = new LibraryModel(5, "B20dccn553", "Quan5", "0333935933", "D20DCQCN01-B");
+        LibraryModel test5 = new LibraryModel(6, "B20dccn553", "Quan6", "0333935933", "D20DCQCN01-B");
+        LibraryModel test6 = new LibraryModel(7, "B20dccn553", "Quan7", "0333935933", "D20DCQCN01-B");
+        LibraryModel test7 = new LibraryModel(8, "B20dccn553", "Quan8", "0333935933", "D20DCQCN01-B");
+        LibraryModel test8 = new LibraryModel(9, "B20dccn553", "Quan9", "0333935933", "D20DCQCN01-B");
+        LibraryModel test9 = new LibraryModel(10, "B20dccn553", "Quan10", "0333935933", "D20DCQCN01-B");
+        LibraryModel test10 = new LibraryModel(11, "B20dccn553", "Quan11", "0333935933", "D20DCQCN01-B");
+        LibraryModel test11 = new LibraryModel(12, "B20dccn553", "Quan12", "0333935933", "D20DCQCN01-B");
+        return FXCollections.observableArrayList(test, test1, test2, test3, test4, test6, test7, test8, test9, test10, test11);
     }
 
 
--- src/main/java/btl_java/manage_library/models/LibraryModel.java
+++ src/main/java/btl_java/manage_library/models/LibraryModel.java
@@ -1,29 +1,34 @@
 package btl_java.manage_library.models;
 
 import com.mysql.cj.conf.StringProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class LibraryModel {
+    private final SimpleStringProperty stt = new SimpleStringProperty();
     private final SimpleStringProperty codeStudent = new SimpleStringProperty();
     private final SimpleStringProperty nameStudent = new SimpleStringProperty();
-    private final SimpleStringProperty bookTook = new SimpleStringProperty();
+    private final SimpleStringProperty classStudent = new SimpleStringProperty();
+    private final SimpleStringProperty phoneNumber = new SimpleStringProperty();
+    private final SimpleStringProperty timeIn = new SimpleStringProperty();
+    private final SimpleStringProperty timeOut = new SimpleStringProperty();
 
-    public LibraryModel(String codeStudent, String nameStudent, String bookTook) {
+    public LibraryModel(int stt, String codeStudent, String nameStudent, String phoneNumber, String classStudent) {
+        this.stt.set(Integer.toString(stt));
         this.codeStudent.set(codeStudent);
         this.nameStudent.set(nameStudent);
-        this.bookTook.set(bookTook);
+        this.timeIn.set(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
+        this.phoneNumber.set(phoneNumber);
+        this.classStudent.set(classStudent);
+        this.timeOut.set("");
     }
-
-//    public Date getTimeIn() {
-//        return timeIn;
-//    }
-//
-//    public Date getTimeOut() {
-//        return timeOut;
-//    }
-
+    public SimpleStringProperty getStt() {
+        return stt;
+    }
     public SimpleStringProperty getCodeStudent() {
         return codeStudent;
     }
@@ -32,18 +37,35 @@ public class LibraryModel {
         return nameStudent;
     }
 
-    public SimpleStringProperty getBookTook() {
-        return bookTook;
+    public SimpleStringProperty getClassStudent() {
+        return classStudent;
+    }
+
+    public SimpleStringProperty getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public SimpleStringProperty getTimeIn() {
+        return timeIn;
+    }
+
+    public SimpleStringProperty getTimeOut() {
+        return timeOut;
+    }
+
+    public void setTimeOut() {
+
     }
 
     @Override
     public String toString() {
         return "LibraryModel{" +
-//                "timeIn=" + timeIn +
-//                ", timeOut=" + timeOut +
-                ", codeStudent='" + codeStudent + '\'' +
-                ", nameStudent='" + nameStudent + '\'' +
-                ", bookTook='" + bookTook + '\'' +
+                "codeStudent=" + codeStudent +
+                ", nameStudent=" + nameStudent +
+                ", timeIn=" + timeIn +
+                ", timeOut=" + timeOut +
+                ", phoneNumber=" + phoneNumber +
+                ", classStudent=" + classStudent +
                 '}';
     }
 }
