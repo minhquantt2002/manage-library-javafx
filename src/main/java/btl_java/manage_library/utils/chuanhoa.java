9e44e7df135fb657e8b6f0c88104f78901f5dcbf
Ha Quang <haquang782002@gmail.com>
2022-11-02 20:37:06 +0700

    Quang-test commit

--- src/main/java/btl_java/manage_library/utils/chuanhoa.java
+++ src/main/java/btl_java/manage_library/utils/chuanhoa.java
@@ -1,16 +1,5 @@
 package btl_java.manage_library.utils;
 
 public class chuanhoa {
-    public String chuanhoaBook(String s){
-        s= s.trim().toLowerCase();
-        s= s.replaceAll("\\s+"," ");
-        String[] temp = s.split(" ");
-        s="";
-        for (int i = 0; i < temp.length; i++) {
-            s+=String.valueOf(temp[i].charAt(0)).toUpperCase()+
-                    temp[i].substring(1);
-            if(i< temp.length-1) s+=" ";
-        }
-        return s;
-    }
+
 }
