7f33991bbbc8a98bd15f86baee41c7655e590474
Ha Quang <haquang782002@gmail.com>
2022-11-03 10:46:06 +0700

    Quang-editDelete and search

--- src/main/java/btl_java/manage_library/controllers/LibraryManagerController.java
+++ src/main/java/btl_java/manage_library/controllers/LibraryManagerController.java
@@ -4,6 +4,7 @@ import btl_java.manage_library.utils.ConnectionUtils;
 import btl_java.manage_library.models.LibraryModel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -82,7 +83,7 @@ public class LibraryManagerController implements Initializable {
         }
         tableViewLbm.setItems(list);
     }
-
+//==========================================INSERTSTUDENT=============================================
     @FXML
     private void insertStudent() {
         // Kiểm tra dữ liệu trước khi thêm sinh viên
@@ -90,30 +91,30 @@ public class LibraryManagerController implements Initializable {
         if (!valid) {
             Alert warn = this.createAlert(Alert.AlertType.WARNING, "Chưa điền đủ thông tin", "", "Điền thông tin", ButtonType.CLOSE);
             warn.show();
-            clear();
-            return;
         }
-        String stmt = "INSERT INTO library_manager (student_code, full_name, class_name, phone_number, time_in, time_out) VALUES (?, ?, ?, ?, ?, ?)";
-        Connection connection = new ConnectionUtils().connectDB();
-        try {
-            String timeInToString = DateTimeFormatter.ofPattern("HH:mm:ss-dd/MM/yyyy").format(LocalDateTime.now());
-            PreparedStatement preparedStatement = connection.prepareStatement(stmt);
-            preparedStatement.setString(1, idStudentField.getText());
-            preparedStatement.setString(2, nameStudentField.getText());
-            preparedStatement.setString(3, classStudentField.getText());
-            preparedStatement.setString(4, phoneNumberStudentField.getText());
-            preparedStatement.setString(5, timeInToString);
-            preparedStatement.setString(6, "");
-            preparedStatement.executeUpdate();
-            System.out.println("Create done a record: " + new LibraryModel(idStudentField.getText(), nameStudentField.getText(),
-                    classStudentField.getText(), phoneNumberStudentField.getText(), timeInToString, timeInToString, ""));
-            this.clear();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        else{
+            String stmt = "INSERT INTO library_manager (student_code, full_name, class_name, phone_number, time_in, time_out) VALUES (?, ?, ?, ?, ?, ?)";
+            Connection connection = new ConnectionUtils().connectDB();
+            try {
+                String timeInToString = DateTimeFormatter.ofPattern("HH:mm:ss-dd/MM/yyyy").format(LocalDateTime.now());
+                PreparedStatement preparedStatement = connection.prepareStatement(stmt);
+                preparedStatement.setString(1, idStudentField.getText());
+                preparedStatement.setString(2, nameStudentField.getText());
+                preparedStatement.setString(3, classStudentField.getText());
+                preparedStatement.setString(4, phoneNumberStudentField.getText());
+                preparedStatement.setString(5, timeInToString);
+                preparedStatement.setString(6, "");
+                preparedStatement.executeUpdate();
+                System.out.println("Create done a record: " + new LibraryModel(idStudentField.getText(), nameStudentField.getText(),
+                        classStudentField.getText(), phoneNumberStudentField.getText(), timeInToString, timeInToString, ""));
+                this.clear();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            setDataTableView();
         }
-        setDataTableView();
-    }
 
+    }
 
 
     @FXML
@@ -168,43 +169,66 @@ public class LibraryManagerController implements Initializable {
         classStudentField.setText("");
         phoneNumberStudentField.setText("");
     }
-//========================================STUDENTOUT===========================================
+
+    //========================================STUDENTOUT===========================================
     @FXML
     private void searchStudent() {
+
         String findID = idStudentField.getText();
-        if(nameStudentField.getText().equals("")&&classStudentField.getText().equals("")&&phoneNumberStudentField.getText().equals("")&& !findID.equals("")){
-            String timeOutToString = DateTimeFormatter.ofPattern("HH:mm:ss-dd/MM/yyyy").format(LocalDateTime.now());
-            String stmt = "UPDATE library_manager SET time_out='"+timeOutToString+"' WHERE student_code='"+findID+"'AND time_out =''";
-            Connection conn = new ConnectionUtils().connectDB();
-            try {
-                PreparedStatement preparedStatement = conn.prepareStatement(stmt);
-                preparedStatement.executeUpdate();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+        if(findID.equals("")){
             setDataTableView();
         }
-//        LibraryModel selected = tableViewLbm.getSelectionModel().getSelectedItem();
-//        if (selected != null) {
-//            Alert warn = this.createAlert(Alert.AlertType.WARNING, "Thêm thông tin cho sinh viên này ? ", "", "Thêm thông tin sinh viên ", ButtonType.CLOSE);
-//            Optional<ButtonType> option = warn.showAndWait();
-//            try {
-//                if (ButtonType.OK == option.get()) {
-//                    String timeOutToString = DateTimeFormatter.ofPattern("HH:mm:ss-dd/MM/yyyy").format(LocalDateTime.now());
-//                    String stmt = "UPDATE library_manager SET time_out='"+timeOutToString+"' WHERE student_code='"+selected.getCodeStudent().getValue()+"'";
-//                    try {
-//                        Connection conn = new ConnectionUtils().connectDB();
-//                        PreparedStatement pstm = conn.prepareStatement(stmt);
-//                        pstm.executeUpdate();
-//                        System.out.println("Insert done a record: " + selected+timeOutToString);
-//                    } catch (SQLException err) {
-//                        System.err.println("Insert : " + err.getMessage());
-//                    }
-//                    setDataTableView();
-//                }
-//            } catch (Exception e) {
-//                System.out.println("error " + e.getMessage());
-//            }
-//        }
+        else{
+            String stmt = "SELECT * FROM library_manager WHERE student_code='"+findID+"'";
+            Connection connection = new ConnectionUtils().connectDB();
+            ObservableList<LibraryModel> list = FXCollections.observableArrayList();
+            ResultSet resultSet;
+            try {
+                int i = 1;
+                resultSet = connection.createStatement().executeQuery(stmt);
+                while (resultSet.next()) {
+                    LibraryModel row = new LibraryModel(
+                            Integer.toString(i),
+                            resultSet.getString(2),
+                            resultSet.getString(3),
+                            resultSet.getString(4),
+                            resultSet.getString(5),
+                            resultSet.getString(6),
+                            resultSet.getString(7)
+                    );
+                    i++;
+                    list.add(row);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            tableViewLbm.setItems(list);
+        }
+
+
+    }
+
+    public void updateStudent() {
+        String findID = idStudentField.getText();
+        if (nameStudentField.getText().equals("") && classStudentField.getText().equals("") && phoneNumberStudentField.getText().equals("") && !findID.equals("")) {
+            Alert warn = this.createAlert(Alert.AlertType.WARNING, "Thêm thông tin cho sinh viên này ? ", "", "Thêm thông tin sinh viên ", ButtonType.CLOSE);
+            Optional<ButtonType> option = warn.showAndWait();
+            try {
+                if (ButtonType.OK == option.get()) {
+                    String timeOutToString = DateTimeFormatter.ofPattern("HH:mm:ss-dd/MM/yyyy").format(LocalDateTime.now());
+                    String stmt = "UPDATE library_manager SET time_out='" + timeOutToString + "' WHERE student_code='" + findID + "'AND time_out =''";
+                    Connection conn = new ConnectionUtils().connectDB();
+                    try {
+                        PreparedStatement preparedStatement = conn.prepareStatement(stmt);
+                        preparedStatement.executeUpdate();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    setDataTableView();
+                }
+            } catch (Exception e) {
+                System.out.println("error " + e.getMessage());
+            }
+        }
     }
 }
