4bee2c14a9180414fb4e30b141d63951c08af69a
Ha Quang <haquang782002@gmail.com>
2022-11-23 07:12:25 +0700

    Quang- new update

--- src/main/java/btl_java/manage_library/controllers/ChartController.java
+++ src/main/java/btl_java/manage_library/controllers/ChartController.java
@@ -2,6 +2,7 @@ package btl_java.manage_library.controllers;
 
 import btl_java.manage_library.models.BorrowDetailModel;
 import btl_java.manage_library.models.BorrowerModel;
+import btl_java.manage_library.utils.AlertWarningUtils;
 import btl_java.manage_library.utils.ConnectionUtils;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -11,6 +12,7 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Button;
+
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -23,25 +25,25 @@ import btl_java.manage_library.models.LibraryModel;
 
 public class ChartController implements Initializable {
     @FXML
-    private Button All;
+    private Button filterall;
     @FXML
-    private ChoiceBox<String> nam;
+    private ChoiceBox<String> year;
 
     @FXML
-    private ChoiceBox<String> thang;
+    private ChoiceBox<String> month;
     @FXML
-    private Button b_refresh;
+    private Button filter;
     @FXML
-    private LineChart<String, Number> chart_S;
+    private LineChart<String, Number> chartBookLine;
 
     @FXML
-    private LineChart<String, Number> chart_Sv;
+    private LineChart<String, Number> chartStudentline;
 
     Connection connection = new ConnectionUtils().connectDB();
-    LinkedHashMap<String, Integer> S_data = new LinkedHashMap<>();
-   LinkedHashMap<String, Integer> B_data = new LinkedHashMap<>();
+    LinkedHashMap<String, Integer> StudentData = new LinkedHashMap<>();
+    LinkedHashMap<String, Integer> BookData = new LinkedHashMap<>();
 
-    public void S_chart_getData(String stmt) {
+    public void StudentgetData(String stmt) {
 
         ArrayList<String> list = new ArrayList<>();
         ResultSet resultSet;
@@ -65,17 +67,17 @@ public class ChartController implements Initializable {
             e.printStackTrace();
         }
         for (String s : list) {
-            if (!S_data.containsKey(s)) {
-                S_data.put(s, 1);
+            if (!StudentData.containsKey(s)) {
+                StudentData.put(s, 1);
             } else {
-                int x = S_data.get(s);
+                int x = StudentData.get(s);
                 x += 1;
-                S_data.put(s, x);
+                StudentData.put(s, x);
             }
         }
     }
 
-    private void B_chart_getData(String stmt) {
+    private void BookgetData(String stmt) {
         ArrayList<String> list = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
         ResultSet resultSet, resultSet2;
@@ -94,7 +96,7 @@ public class ChartController implements Initializable {
         }
         for (String l : list) {
             try {
-                resultSet2 = connection.createStatement().executeQuery("SELECT * FROM borrowed_book_detail where borrower_id='"+l+"'");
+                resultSet2 = connection.createStatement().executeQuery("SELECT * FROM borrowed_book_detail where borrower_id='" + l + "'");
                 while (resultSet2.next()) {
                     BorrowDetailModel row = new BorrowDetailModel(
                             resultSet2.getString(1),
@@ -113,67 +115,68 @@ public class ChartController implements Initializable {
 
         }
         for (String q : list2) {
-            if (!B_data.containsKey(q)) {
-                B_data.put(q, 1);
+            if (!BookData.containsKey(q)) {
+                BookData.put(q, 1);
             } else {
-                int x =B_data.get(q);
+                int x = BookData.get(q);
                 x += 1;
-                B_data.put(q, x);
+                BookData.put(q, x);
             }
         }
 
     }
-    String[] C_thang={"Tháng 01","Tháng 02","Tháng 03","Tháng 04","Tháng 05","Tháng 06","Tháng 07","Tháng 08","Tháng 09","Tháng 10","Tháng 11","Tháng 12"};
-    String[] C_nam={"năm 2020","năm 2021","năm 2022"};
+
+    String[] ChooseMonth = {"Tháng 01", "Tháng 02", "Tháng 03", "Tháng 04", "Tháng 05", "Tháng 06", "Tháng 07", "Tháng 08", "Tháng 09", "Tháng 10", "Tháng 11", "Tháng 12"};
+    String[] ChooseYear = {"năm 2020", "năm 2021", "năm 2022"};
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        thang.getItems().addAll(C_thang);
-        nam.getItems().addAll(C_nam);
-        S_chart_getData("SELECT * FROM library_manager");
-        B_chart_getData("SELECT * FROM borrower");
-        chart_Sv();
-        chartB();
-        b_refresh.setOnAction(this::refresh);
-        All.setOnAction(this::All);
+        month.getItems().addAll(ChooseMonth);
+        year.getItems().addAll(ChooseYear);
+        StudentgetData("SELECT * FROM library_manager");
+        BookgetData("SELECT * FROM borrower");
+        ChartStudent();
+        ChartBook();
+        filter.setOnAction(this::Refinement);
+        filterall.setOnAction(this::RefinementAll);
     }
 
 //    data chart Lượng sinh viên vào ra thư viện :_______________________________________________________
 
 
     //    Luong sv vao ra:
-    private void chart_Sv() {
+    private void ChartStudent() {
         XYChart.Series series = new XYChart.Series();
 
-        for (Map.Entry<String, Integer> x : S_data.entrySet()) {
+        for (Map.Entry<String, Integer> x : StudentData.entrySet()) {
             String s = x.getKey();
             int n = x.getValue();
             series.getData().add(new XYChart.Data(s, n));
 //            System.out.println(s + " " + n);
-
         }
-        chart_Sv.getData().add(series);
+        chartStudentline.getData().add(series);
 
     }
-    private void chart_Sv2(String s){
-        XYChart.Series series=new XYChart.Series();
-        for(Map.Entry<String,Integer> x:S_data.entrySet()){
-            String l=x.getKey().substring(3);
-            int n=x.getValue();
-            if(l.equals(s)){
-                series.getData().add(new XYChart.Data(x.getKey(),n));
+
+    private void ChartStudentrefinement(String s) {
+        XYChart.Series series = new XYChart.Series();
+        for (Map.Entry<String, Integer> x : StudentData.entrySet()) {
+            String l = x.getKey().substring(3);
+            int n = x.getValue();
+            if (l.equals(s)) {
+                series.getData().add(new XYChart.Data(x.getKey(), n));
             }
         }
-        chart_Sv.getData().add(series);
+        chartStudentline.getData().add(series);
     }
 
 
 //    Lượng sách mượn :__________________________________________________________________________-
 
-    private void chartB() {
+    private void ChartBook() {
         XYChart.Series series = new XYChart.Series();
 
-        for (Map.Entry<String, Integer> x : B_data.entrySet()) {
+        for (Map.Entry<String, Integer> x : BookData.entrySet()) {
             String s = x.getKey();
             int n = x.getValue();
             series.getData().add(new XYChart.Data(s, n));
@@ -181,61 +184,60 @@ public class ChartController implements Initializable {
 
         }
 
-        chart_S.getData().add(series);
+        chartBookLine.getData().add(series);
 
     }
 
-    private void chartB2(String s){
-        XYChart.Series series=new XYChart.Series();
-        for(Map.Entry<String,Integer> x:B_data.entrySet()){
-            String l=x.getKey().substring(3);
-            int n=x.getValue();
-            if(l.equals(s)){
-                series.getData().add(new XYChart.Data(x.getKey(),n));
+    private void ChartBookrefinement(String s) {
+        XYChart.Series series = new XYChart.Series();
+        for (Map.Entry<String, Integer> x : BookData.entrySet()) {
+            String l = x.getKey().substring(3);
+            int n = x.getValue();
+            if (l.equals(s)) {
+                series.getData().add(new XYChart.Data(x.getKey(), n));
             }
         }
-        chart_S.getData().add(series);
+        chartBookLine.getData().add(series);
     }
 
 
+    //    refresh
+    private void Refinement(ActionEvent e) {
+        chartBookLine.setData(FXCollections.observableArrayList());
+        chartStudentline.setData(FXCollections.observableArrayList());
+        StudentData.clear();
+        BookData.clear();
+        StudentgetData("SELECT * FROM library_manager");
+        BookgetData("SELECT * FROM borrower");
 
-
-//    refresh
-    private void refresh(ActionEvent e){
-        chart_S.setData(FXCollections.observableArrayList());
-        chart_Sv.setData(FXCollections.observableArrayList());
-        S_data.clear();
-        B_data.clear();
-        S_chart_getData("SELECT * FROM library_manager");
-        B_chart_getData("SELECT * FROM borrower");
-
-        String n=nam.getValue();
-        String t=thang.getValue();
-        String kt=t+n;
-
-        if(kt.equals("nullnull")){
-            chartB();
-            chart_Sv();
-        }  else {
-            String loc=t.substring(6)+"/"+n.substring(4);
-            chart_Sv2(loc);
-            chartB2(loc);
+        String n = year.getValue();
+        String t = month.getValue();
+        String kt = t + n;
+//        System.out.println(t+" "+n);
+        if (n == null && t == null) {
+            ChartBook();
+            ChartStudent();
+        } else if (n == null || t == null) {
+            new AlertWarningUtils().showAlertWarning("Chưa đủ bộ lọc!");
+        } else {
+            String loc = t.substring(6) + "/" + n.substring(4);
+            ChartStudentrefinement(loc);
+            ChartBookrefinement(loc);
         }
 
     }
 
-    private void All(ActionEvent e){
-        chart_S.setData(FXCollections.observableArrayList());
-        chart_Sv.setData(FXCollections.observableArrayList());
-        S_data.clear();
-        B_data.clear();
-        S_chart_getData("SELECT * FROM library_manager");
-        B_chart_getData("SELECT * FROM borrower");
-        chartB();
-        chart_Sv();
-        thang.setValue(null);
-        nam.setValue(null);
-
+    private void RefinementAll(ActionEvent e) {
+        chartBookLine.setData(FXCollections.observableArrayList());
+        chartStudentline.setData(FXCollections.observableArrayList());
+        StudentData.clear();
+        BookData.clear();
+        StudentgetData("SELECT * FROM library_manager");
+        BookgetData("SELECT * FROM borrower");
+        ChartBook();
+        ChartStudent();
+        month.setValue(null);
+        year.setValue(null);
     }
 
 }
